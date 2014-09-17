8883b45daa125a94492cc8946383520e002eed46
xlplbo <booxlp@gmail.com>
2014-09-18 00:38:19 +0800

    modify  lua script

--- Lua2Game/Game/script/test.lua
+++ Lua2Game/Game/script/test.lua
@@ -4,10 +4,10 @@ function main(szName, num1, num2)
 	print("main()", szName, num1, num2);
 	local nRet = SayHello(szName, num1, num2);
 	print("nRet =", nRet);
-	local nRand = math.random(100);
+	local nRand = math.random(10000);
 	print("nRand =", nRand)
 	test();
-	if nRand > 99 then
+	if nRand > 9999 then
 		StopGame();
 	end
 	return 1;
